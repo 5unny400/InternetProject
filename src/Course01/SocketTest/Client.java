cd1b89b6ce29f47ad295c9efc30620499334a4ca
5unny400 <3407601546@qq.com>
2020-11-04 20:32:07 +0800

    Internet01

--- src/Course01/SocketTest/Client.java
+++ src/Course01/SocketTest/Client.java
@@ -1,13 +1,35 @@
 package Course01.SocketTest;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class Client {
     /*
      * 一般主动连接
      * 1、创建Socket实例
-     * 2、
-     * 3、
-     * 4、
+     * 2、连接服务端端口IP+端口，connect
+     * 3、进行IO操作
+     * 4、关闭资源
      */
+    public static void main(String[] args) throws IOException {
+        //c创建socket实例
+        Socket socket = new Socket();
+        System.out.println("客户端启动！");
 
+        //连接服务器端口IP+端口
+        socket.connect(new InetSocketAddress("127.0.0.1",6666));
+        System.out.println("连接服务器成功！");
+
+        String s = "你好！";
+        //进行IO操作
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write(s.getBytes());
+
+        //断开连接关闭资源
+        socket.close();
+        System.out.println("客户端资源关闭！");
+    }
 
 }
--- src/Course01/SocketTest/Server.java
+++ src/Course01/SocketTest/Server.java
@@ -1,6 +1,8 @@
 package Course01.SocketTest;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -13,7 +15,6 @@ public class Server {
      * 3、监听客户端的连接
      * 4、获取客户端连接的实例
      * 5、进行 IO操作
-     *
      */
     public static void main(String[] args) throws IOException {
         //创建ServerSocket实例
@@ -21,11 +22,22 @@ public class Server {
 
         //绑定端口6666 - 65536
         serverSocket.bind(new InetSocketAddress(6666));
+        System.out.println("服务器启动！");
 
         //监听客户端的连接accept
         final Socket accept = serverSocket.accept();
+        System.out.println("有客户端连接！");
+
 
         //进行IO 操作
+        //input 源-》当前程序         output当前程序到源
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(accept.getInputStream()));
+        String msg = bufferedReader.readLine();
 
+        System.out.println("IO 操作：" + msg);
+
+        //关闭源
+        serverSocket.close();
+        accept.close();
     }
 }
