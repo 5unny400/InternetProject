86cfefb63da2296c5c59639d8ca4f2fb6ee1b696
5unny400 <3407601546@qq.com>
2020-11-25 15:38:58 +0800

    Internet06

--- src/Course03/NIO/Exercise/NIOClient.java
+++ src/Course03/NIO/Exercise/NIOClient.java
@@ -10,99 +10,101 @@ import java.util.Iterator;
 import java.util.Scanner;
 
 public class NIOClient {
+    private Selector selector = null;
+    private SocketChannel socketChannel;
 
-    public static void main(String[] args) {
+    private void startClient(){
         try {
-
-            Selector selector = null;
             //创建SocketChannel通道
-            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel = SocketChannel.open();
 
             //设置socketChannel为非阻塞
             socketChannel.configureBlocking(false);
 
-            //new Thread(new NIOClientMsg(socketChannel)).start();
+            //实例化复用器
+            selector = Selector.open();
+            //连接服务端,该connect不会阻塞，会立即返回 boolean true:连接成功  false：表示还未连接成功
+            if (!socketChannel.connect(new InetSocketAddress("127.0.0.1", 6666))){
+                //表示连接不成功 当前正在连接,将当前的可连接事件交给内核帮助监听
+                socketChannel.register(selector, SelectionKey.OP_CONNECT);
 
+                //等待连接完成
+                selector.select();
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey selectionKey = iterator.next();
+                    iterator.remove();
 
-                //实例化复用器
-                selector = Selector.open();
-                //连接服务端,该connect不会阻塞，会立即返回 boolean true:连接成功  false：表示还未连接成功
-                if (!socketChannel.connect(new InetSocketAddress("127.0.0.1", 6666))){
-                    //表示连接不成功 当前正在连接,将当前的可连接事件交给内核帮助监听
-                    socketChannel.register(selector, SelectionKey.OP_CONNECT);
-
-                    //等待连接完成
-                    selector.select();
-                    Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-                    while (iterator.hasNext()) {
-                        SelectionKey selectionKey = iterator.next();
-                        iterator.remove();
-
-                        if (selectionKey.isConnectable()) socketChannel.finishConnect();
-                    }
+                    if (selectionKey.isConnectable()) socketChannel.finishConnect();
                 }
+            }
 
-                //要么是连接成功
-                //给服务端发送数据
-                Scanner scanner = new Scanner(System.in);
-                String msg = null;
+            //要么是连接成功
+            //给服务端发送数据
+            Scanner scanner = new Scanner(System.in);
+            String msg;
 
-                //注册读事件
-                socketChannel.register(selector,SelectionKey.OP_READ);
+            //注册读事件
+            socketChannel.register(selector,SelectionKey.OP_READ);
 
-                ByteBuffer buffer = ByteBuffer.allocate(1024);
-                while ((msg =scanner.nextLine()) != null) {
-                    //重复性读操作
-                    buffer.clear();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            while ((msg =scanner.nextLine()) != null) {
+                //重复性读操作
+                buffer.clear();
 //                msg +="\n";
-                    //将数据写入到Buffer中
-                    buffer.put((msg+"\n").getBytes());
+                //将数据写入到Buffer中
+                buffer.put((msg+"\n").getBytes());
 
 
-                    buffer.flip();//为读取buffer转换
-                    //将数据从 Buffer中写入channel通道，对于Buffer而言，是读取数据
-                    socketChannel.write(buffer);
+                buffer.flip();//为读取buffer转换
+                //将数据从 Buffer中写入channel通道，对于Buffer而言，是读取数据
+                socketChannel.write(buffer);
 
-                    //等内核数据准备完成（阻塞）
-                    selector.select();
+                //等内核数据准备完成（阻塞）
+                selector.select();
 
-                    Iterator <SelectionKey> iterator = selector.selectedKeys().iterator();
-                    while (iterator.hasNext()) {
-                        SelectionKey selectionKey = iterator.next();
-                        iterator.remove();
-                        if(!selectionKey.isValid()) continue;
-                        if (selectionKey.isReadable()) {
-                            SocketChannel channel = (SocketChannel) selectionKey.channel();
+                Iterator <SelectionKey> iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey selectionKey = iterator.next();
+                    iterator.remove();
+                    //事件无效的话
+                    if(!selectionKey.isValid()) continue;
+                    //如果是可读事件
+                    if (selectionKey.isReadable()) {
+                        SocketChannel channel = (SocketChannel) selectionKey.channel();
 
-                            ByteBuffer allocate = ByteBuffer.allocate(1024);
-                            //将数据从通道写入Buffer
-                            channel.read(allocate);
+                        ByteBuffer allocate = ByteBuffer.allocate(1024);
+                        //将数据从通道写入Buffer
+                        channel.read(allocate);
 
-                            //读写模式切换
-                            allocate.flip();//之后要从buffer读取
-                            //remaining 实际读取的数据长度
-                            byte[] bytes = new byte[allocate.remaining()];
-                            //将Buffer数据读到byte数组中
-                            allocate.get(bytes);
-                            //转换信息为String
-                            String recv = new String(bytes);
+                        //读写模式切换
+                        allocate.flip();//之后要从buffer读取
+                        //remaining 实际读取的数据长度
+                        byte[] bytes = new byte[allocate.remaining()];
+                        //将Buffer数据读到byte数组中
+                        allocate.get(bytes);
+                        //转换信息为String
+                        String recv = new String(bytes);
 
-                            System.out.println(recv);
-                        }
-                    }
-                    //判断是否结束
-                    if ("exit".equals(msg)) {
-                        break;
+                        System.out.println(recv);
                     }
                 }
-                //关闭资源
-                selector.close();
-                socketChannel.close();
-
+                //判断业务是否结束
+                if ("exit".equals(msg)) {
+                    break;
+                }
+            }
+            //关闭资源
+            selector.close();
+            socketChannel.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void main(String[] args) {
+        NIOClient nioClient = new NIOClient();
+        nioClient.startClient();
     }
 
 }
--- src/Course03/NIO/Exercise/NIOServerMsgChild.java
+++ src/Course03/NIO/Exercise/NIOServerMsgChild.java
@@ -3,59 +3,83 @@ package Course03.NIO.Exercise;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class NIOServerMsgChild implements Runnable {
-    SelectionKey selectionKey;
+    private Selector selector;
+    //SocketChannel socketChannel;
 
-    public NIOServerMsgChild(SelectionKey selectionKey) {
-        this.selectionKey = selectionKey;
+    public NIOServerMsgChild(SocketChannel socketChannel) throws IOException {
+        //  this.socketChannel = socketChannel;
+
+        socketChannel.configureBlocking(false);
+
+        //创建selector复用器
+        selector = Selector.open();
+        //将SocketChannel注册到复用器上，并关注读事件
+        socketChannel.register(selector, SelectionKey.OP_READ);
     }
 
     @Override
     public void run() {
         try {
-            if (selectionKey.isReadable()) {
-                //当前有可读事件发生
-                SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+            //等待系统返回已完成的事件
+            //select()本身是会阻塞，等系统告诉用户空间那些事件已经准备就绪，返回结果表示已准备完成的事件个数
+            while (selector.select() > 0) {
+                //感兴趣事件集合（指的就是注册到selector中的事件）
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                Iterator<SelectionKey> iterator = selectionKeys.iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey selectionKey = iterator.next();
+                    //删除掉已经完成的事件
+                    iterator.remove();
 
-                //读数据 堆上创建指定大小的缓冲
-                ByteBuffer buffer = ByteBuffer.allocate(1024);
-                //往Buffer中写数据
-                int read = socketChannel.read(buffer);
-                if (read == -1) {
-                    //客户端已经关闭
-                    socketChannel.close();
-                    //处理下一个客户端
-                    return;
+                    if (selectionKey.isReadable()) {
+                        //当前有可读事件发生
+                        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+
+                        //读数据 堆上创建指定大小的缓冲
+                        ByteBuffer buffer = ByteBuffer.allocate(1024);
+                        //往Buffer中写数据
+                        int read = socketChannel.read(buffer);
+                        if (read == -1) {
+                            //客户端已经关闭
+                            socketChannel.close();
+                            //处理下一个客户端
+                            continue;
+                        }
+
+                        buffer.flip();//为读取buffer转换
+                        byte[] bytes = new byte[buffer.remaining()];
+                        //读取buff数据
+                        buffer.get(bytes);
+                        //转为String类型
+                        String msg = new String(bytes);
+
+                        System.out.println("客户端：" + socketChannel.getRemoteAddress() + " 发送数据" + msg);
+
+                        //返回信息，表示服务端收到
+                        String recv = "[echo]:" + msg;
+                        //先将Buffer清空
+                        buffer.clear();
+                        //往Buffer写数据
+                        buffer.put(recv.getBytes());
+
+                        buffer.flip();//为读取buffer转换
+                        //将Buffer数据读到channel通道，
+                        socketChannel.write(buffer);
+
+                        //业务断开
+                        if ("exit".equals(msg)) {
+                            socketChannel.close();
+                            System.out.println("断开连接了！");
+                        }
+                    }
                 }
-
-                buffer.flip();//为读取buffer转换
-                byte[] bytes = new byte[buffer.remaining()];
-                //读取buff数据
-                buffer.get(bytes);
-                //转为String类型
-                String msg = new String(bytes);
-
-                System.out.println("客户端：" + socketChannel.getRemoteAddress() + " 发送数据" + msg);
-
-                //返回信息，表示服务端收到
-                String recv = "[echo]:" + msg;
-                //先将Buffer清空
-                buffer.clear();
-                //往Buffer写数据
-                buffer.put(recv.getBytes());
-
-                buffer.flip();//为读取buffer转换
-                //将Buffer数据读到channel通道，
-                socketChannel.write(buffer);
-
-                //业务断开
-                if ("exit".equals(msg)) {
-                    socketChannel.close();
-                    System.out.println("断开连接了！");
-                    return;
-                }
+                //来循环使事件完成
             }
         } catch (IOException e) {
             e.printStackTrace();
